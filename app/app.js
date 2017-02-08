d89b3951660ead137919268b978fef8480f58afd
Angel Carvajal <angel_adcm@hotmail.com>
2017-02-07 22:25:30 -0600

    F-04: Inject new directives, services and factories

--- app/app.js
+++ app/app.js
@@ -1,6 +1,9 @@
 import { urlRouterProvider, stateProvider } from './config.js';
 import resultsController from './results/controller.js';
 import navBarDirective from './directives/navBar/directive.js';
+import adCardDirective from './directives/adCard/directive.js';
+import resultsService from './services/results.js';
+import globals from './factories/globals.js';
 
 const {
 	module
@@ -17,13 +20,16 @@ app.config(['$urlRouterProvider', urlRouterProvider]);
 app.config(['$stateProvider', stateProvider]);
 
 //Controllers
-app.controller('resultsController', ['$scope', resultsController]);
+app.controller('resultsController', ['$scope', 'resultsService', 'globals', resultsController]);
 
 //Services
+app.service('resultsService', ['$http', 'globals', resultsService]);
 
 //Factories
+app.factory('globals', ['$log', 'LxNotificationService', globals]);
 
 //Directives
 app.directive('navBar', [navBarDirective]);
+app.directive('adCard', [adCardDirective]);
 
 export default app;
